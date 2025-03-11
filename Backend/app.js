18b4a449cd294525fee7dd7a3d14a13760eeb31a
Pasindya <pasindyachavindi13@gmail.com>
2025-03-11 14:04:59 +0530

    added db connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res, next) =>{
+    res.send("It is working correctly");
+})
+
+//database connection
+mongoose.connect("mongodb+srv://travel:1234@cluster0.nitw6zk.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
